6b27c4e04c0fbf96188a30acb49d5adb0d2b4346
ytulenov <86948036+ytulenov@users.noreply.github.com>
2024-05-06 00:30:36 -0700

    ghjkl

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -22,13 +22,15 @@ const HeroSection = () => {
             <br></br>
             <TypeAnimation
               sequence={[
-                "Judy",
+                "Arina Kenbayeva",
                 1000,
-                "Web Developer",
+                "Financier",
                 1000,
-                "Mobile Developer",
+                "",
                 1000,
-                "UI/UX Designer",
+                "Accountant",
+                1000,
+                "Financial Analytic",
                 1000,
               ]}
               wrapper="span"
@@ -37,8 +39,7 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-            voluptuous.
+            I am developing statistical models to evaluate performance of the business
           </p>
           <div>
             <Link
@@ -47,32 +48,32 @@ const HeroSection = () => {
             >
               Hire Me
             </Link>
-            <Link
-              href="/"
+            <a
+              href="./Arina-9.pdf"
+              target="_blank"
+              alt='My CV'
+              rel= 'noopener noreferrer'
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
+          
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                 Download CV
               </span>
-            </Link>
+            </a>
           </div>
         </motion.div>
         <motion.div
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.5 }}
-          className="col-span-4 place-self-center mt-4 lg:mt-0"
-        >
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
-            <Image
-              src="/images/hero-image.png"
-              alt="hero image"
-              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              width={300}
-              height={300}
-            />
-          </div>
-        </motion.div>
+  initial={{ opacity: 0, scale: 0.5 }}
+  animate={{ opacity: 1, scale: 1 }}
+  transition={{ duration: 0.5 }}
+  className="col-span-4 place-self-center mt-4 lg:mt-0 rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative"
+  style={{
+    backgroundImage: "url(/images/WhatsAppImage20240501at024222.jpeg)",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  }}
+>
+</motion.div>
       </div>
     </section>
   );
