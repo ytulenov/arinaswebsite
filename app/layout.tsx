2ceaad05f132f55c728e23e7b1d08b0367984033
ytulenov <86948036+ytulenov@users.noreply.github.com>
2026-07-16 20:06:31 -0700

    fbd

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter, Playfair_Display } from 'next/font/google'
 import { cn } from '@/lib/utils'
 
 import './globals.css'
+import 'katex/dist/katex.min.css' 
 import Providers from '@/components/providers'
 import Header from '@/components/header'
 import Footer from '@/components/footer'
--- components/mdx-content.tsx
+++ components/mdx-content.tsx
@@ -1,11 +1,12 @@
-import { JSX } from 'react'
-import { highlight } from 'sugar-high'
 import { MDXRemote, MDXRemoteProps } from 'next-mdx-remote/rsc'
-
+import remarkMath from 'remark-math'
+import rehypeKatex from 'rehype-katex'
+import { highlight } from 'sugar-high'
 import Counter from '@/components/counter'
 
-function Code({ children, ...props }: any) {
-  let codeHTML = highlight(children)
+function Code({ children, className, ...props }: any) {
+  // Don't let sugar-high touch math blocks (remark-math handles them)
+  const codeHTML = highlight(children)
   return <code dangerouslySetInnerHTML={{ __html: codeHTML }} {...props} />
 }
 
@@ -14,13 +15,19 @@ const components = {
   Counter
 }
 
-export default function MDXContent(
-  props: JSX.IntrinsicAttributes & MDXRemoteProps
-) {
+export default function MDXContent(props: MDXRemoteProps) {
   return (
     <MDXRemote
       {...props}
       components={{ ...components, ...(props.components || {}) }}
+      options={{
+        mdxOptions: {
+          remarkPlugins: [remarkMath],
+          rehypePlugins: [rehypeKatex]
+        },
+        // merge with any options passed in props if you have them
+        ...(props.options || {})
+      }}
     />
   )
 }
